a3276172400fcb8a33733f5ffc473f9c233c1851
Hongbin Miao <zslrmhb@gmail.com>
2025-05-31 22:07:48 -0700

    delete workflow

--- frontend/.svelte-kit/ambient.d.ts
+++ frontend/.svelte-kit/ambient.d.ts
@@ -70,7 +70,7 @@ declare module '$env/static/private' {
  * ```
  */
 declare module '$env/static/public' {
-	
+	export const PUBLIC_API_URL: string;
 }
 
 /**
@@ -140,6 +140,7 @@ declare module '$env/dynamic/private' {
  */
 declare module '$env/dynamic/public' {
 	export const env: {
+		PUBLIC_API_URL: string;
 		[key: `PUBLIC_${string}`]: string | undefined;
 	}
 }
